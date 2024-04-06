7918263adfa1939796b439756a21f936c901c554
Ayyappa Reddy <areddy@talentquest.com>
2024-04-06 09:26:05 +0530

    jwt token generate simple code web api

---
+++ JWT_Token/Controllers/LoginController.cs
@@ -0,0 +1,64 @@
+﻿using JWT_Token.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace JWT_Token.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LoginController : ControllerBase
+{
+    private IConfiguration _config;
+
+    public LoginController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    private Users AuthenticateUser(Users users)
+    {
+        Users _user = new Users();
+        if(users.UserName == "admin" && users.Password == "reddy@011") 
+        {
+            _user = new Users { UserName = "Ayyappa Reddy", };
+        }
+
+        return _user;
+    }
+
+    private string GenerateToken(Users users)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256) ;
+
+        var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+            _config["Jwt:Audience"],
+            null,
+            expires: DateTime.Now.AddMinutes(1),
+            signingCredentials: credentials
+          );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    [AllowAnonymous]
+    [HttpPost]
+    public IActionResult Login(Users users)
+    {
+        IActionResult response = Unauthorized();
+
+        var user = AuthenticateUser(users);
+        if(user is not null) 
+        {
+            var token = GenerateToken(users);
+            response = Ok(new { token = token });
+        }
+
+        return response;
+    }
+}
+
---
+++ JWT_Token/Models/Users.cs
@@ -0,0 +1,8 @@
+﻿namespace JWT_Token.Models;
+
+    public class Users
+    {
+         public string UserName { get; set; } = string.Empty;
+         public string Password { get; set; } = string.Empty;
+    }
+
